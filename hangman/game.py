acb9991b6f8837655753d5692bad65e34af6d4c1
kufopo <kufopo@gmail.com>
2018-06-02 22:03:38 +0000

    2 of 3 done

--- hangman/game.py
+++ hangman/game.py
@@ -26,7 +26,48 @@ class GuessAttempt(object):
 
 
 class GuessWord(object):
-    pass
+    def __init__(self,word):
+        
+        if len(word) == 0:
+            raise InvalidWordException()
+        
+        self.answer = word
+        self.masked = '*' * len(self.answer)
+        
+        
+    def discovery(self,  chara ):
+       
+        holder = ''
+        for j in range(len(self.answer)):
+            wordLetter = self.answer[j].lower()
+            maskedLetter =self. masked[j]
+    
+            if maskedLetter != '*':
+                holder += maskedLetter
+            elif chara.lower() == wordLetter:
+                holder += wordLetter
+    
+            else:
+                holder += '*'
+        return holder    
+    
+    
+    
+        
+    def perform_attempt (self, chara):
+        if len(chara)>1:
+            raise InvalidGuessedLetterException()
+            
+        
+            
+        if chara.lower() in self.answer.lower():
+            
+            self.masked = self.discovery( chara)
+            
+            return GuessAttempt(chara, hit=True )
+        else:
+            return GuessAttempt(chara, miss=True )
+        
 
 
 class HangmanGame(object):
