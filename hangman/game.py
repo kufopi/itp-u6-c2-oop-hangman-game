f3eb6a109edbf152c034c042c578b4222a301a0a
kufopo <kufopo@gmail.com>
2018-06-02 14:18:07 +0000

    stage 1 of 3 complete

--- hangman/game.py
+++ hangman/game.py
@@ -2,7 +2,27 @@ from .exceptions import *
 
 
 class GuessAttempt(object):
-    pass
+    
+    def __init__(self, chara, hit = None, miss = None):
+        self.chara = chara
+        self.hit =hit
+        self.miss = miss
+        
+        if  hit is True and  miss is True:
+            raise InvalidGuessAttempt()
+    
+    def is_hit(self):
+        if self.hit == True:
+            return True
+        return False
+        
+        
+        
+    def is_miss(self):
+        if self.miss == True:
+            return True
+        return False
+        
 
 
 class GuessWord(object):
