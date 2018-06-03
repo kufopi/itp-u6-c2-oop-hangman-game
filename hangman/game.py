67d6bef093053b5f982e7820f8164c46e67166a5
kufopo <kufopo@gmail.com>
2018-06-03 16:27:30 +0000

    Back-breaking but learnt a lot

--- hangman/game.py
+++ hangman/game.py
@@ -1,4 +1,5 @@
 from .exceptions import *
+import random
 
 
 class GuessAttempt(object):
@@ -71,4 +72,61 @@ class GuessWord(object):
 
 
 class HangmanGame(object):
-    pass
+    WORD_LIST = ['rmotr', 'python', 'awesome']
+    def __init__(self, word_list = None, number_of_guesses = 5 ):
+        
+        if  not word_list :
+            word_list =self.WORD_LIST
+        else:
+            self.word_list = word_list
+        self.number_of_guesses = number_of_guesses
+        self.remaining_misses = number_of_guesses
+        self.word = GuessWord(self.select_random_word(word_list))
+        self.previous_guesses = []
+        
+    @classmethod
+    def select_random_word(cls, word_list):
+        if len(word_list) == 0:
+            raise InvalidListOfWordsException()
+        chosenWord = random.choice(word_list)
+        return chosenWord
+        
+        
+    def guess(self,character):
+        
+        if self.is_finished():
+            raise GameFinishedException()
+        
+        attempt = self.word.perform_attempt(character)
+        self.previous_guesses.append(character.lower())
+        
+        if attempt.is_miss():
+            self.remaining_misses -=1
+            
+            if self.remaining_misses == 0:
+                raise GameLostException()
+        
+        if '*' not in self.word.masked:
+            
+            raise GameWonException()
+        
+        return attempt
+    
+    def is_finished(self):
+        if self.is_won() or self.is_lost():
+            return True
+        return False
+        
+    def is_lost(self):  
+        if self.remaining_misses == 0:
+            return True
+        return False
+        
+        
+    def is_won(self):
+        
+        if '*' not in self.word.masked:
+            return True
+        return False
+    
+    
